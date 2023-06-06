9e13588ab18057b155bed65cd9cf55c7afb301cc
Umar farooq <135575930+Umarkahhhn@users.noreply.github.com>
2023-06-06 20:28:26 +0500

    Add files via upload

---
+++ Virtual Functions.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+class Shape {
+public:
+    virtual void draw() {
+        std::cout << "Drawing a shape." << std::endl;
+    }
+};
+
+class Circle : public Shape {
+public:
+    void draw() override {
+        std::cout << "Drawing a circle." << std::endl;
+    }
+};
+
+class Square : public Shape {
+public:
+    void draw() override {
+        std::cout << "Drawing a square." << std::endl;
+    }
+};
+
+int main() {
+    Shape* shape1 = new Circle();
+    Shape* shape2 = new Square();
+
+    shape1->draw(); // Calls the draw() function of Circle
+    shape2->draw(); // Calls the draw() function of Square
+
+    delete shape1;
+    delete shape2;
+
+    return 0;
+}
